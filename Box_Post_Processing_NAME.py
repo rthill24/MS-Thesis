bed52bd149e7cc29644dadb503e5a88e7e13a547
rthill24 <rthill@umich.edu>
2024-11-01 22:19:36 -0400

    podt_processing_rewrite

--- Box_Post_Processing_NAME.py
+++ Box_Post_Processing_NAME.py
@@ -1,17 +1,21 @@
-import post_process
+import post_process_rewrite
 import matplotlib.pyplot as plt
+import time
 
 dbname = "C:/Users/rthill/Documents/MS-Thesis/Optimizer_Output"
-testobj = post_process.NSGA_PostProcess(dbname)
+testobj = post_process_rewrite.NSGA_PostProcess(dbname)
 optvar_front = testobj.getFront(100,0,[1,2])
 optvars = testobj.getIndVariables(optvar_front)
 
 #generate just Pareto front for single generation
-plot_pareto = testobj.SingleFront(dbname, 10,1, [1,2], 1)
-plt.show()
+plot_pareto = testobj.SingleFront(10, 0, [1,2], 1.3, "SingleGen_Pareto_Plot")
+plt.show(block=False)
+plt.close()
 
 #generate and show all fronts for single generation
-plot_data = testobj.GenPlot2D(10, [1,2], [1,1], "SingleGen_All_Fronts_Plot")
-plt.show()
+plot_data = testobj.GenPlot2D(10, [1,2], [1,1], "SingleGen_All_Fronts_Plot", True)
+plt.show(block=False)
+plt.close()
 
-movie = testobj.ObjMovie(1,10,[1,2],[1,1],"All_Fronts_Movie")
+#generate a gif to show front progression over generations
+movie = testobj.ObjMovie(1,10,[1,2],[1.3,1.3],"All_Fronts_Movie", False)
