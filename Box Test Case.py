658d8a5838db28761bb83a18439a3bc656895d8a
rthill24 <rthill@umich.edu>
2024-11-21 15:34:22 -0500

    changed box test case

--- Box Test Case.py
+++ Box Test Case.py
@@ -68,10 +68,10 @@ ornt_bot = 0
 qloc_bot = ['NA','NA','NA']
 eta_bot = 0
 
-# Dimensions for TPanel_trans_side
+""" # Dimensions for TPanel_trans_side
 B_side  =  1
 L_side = 1
-nstiff_side = 8
+nstiff_side = 1
 ntrans_side = 1
 tp_side = 0.004
 tw_side = 0.005
@@ -90,7 +90,7 @@ eta_side = 0
 # Dimensions for TPanel_trans_top
 B_top  =  1
 L_top = 1
-nstiff_top = 8
+nstiff_top = 1
 ntrans_top = 1
 tp_top = 0.004
 tw_top = 0.005
@@ -104,18 +104,18 @@ tfb_top = 0.05
 sloc_top = [0,B_side,0]
 ornt_top = 180
 qloc_top = ['NA','NA','NA']
-eta_top = 0
+eta_top = 0  """
 
 # Creation of TPanel_trans
 test_panel_bot = TPanel_trans.TPanel_trans(B_bot,L_bot,nstiff_bot,ntrans_bot,tp_bot,\
                                         tw_bot,hw_bot,tf_bot,bf_bot,twh_bot,twt_bot,\
                                         tft_bot,tfb_bot,sloc_bot,ornt_bot,qloc_bot,pmatl,smatl,tmatl,eta_bot)
-test_panel_side = TPanel_trans.TPanel_trans(B_side,L_side,nstiff_side,ntrans_side,tp_side,\
+""" test_panel_side = TPanel_trans.TPanel_trans(B_side,L_side,nstiff_side,ntrans_side,tp_side,\
                                         tw_side,hw_side,tf_side,bf_side,twh_side,twt_side,\
                                         tft_side,tfb_side,sloc_side,ornt_side,qloc_side,pmatl,smatl,tmatl,eta_side)
 test_panel_top = TPanel_trans.TPanel_trans(B_top,L_top,nstiff_top,ntrans_top,tp_top,\
                                         tw_top,hw_top,tf_top,bf_top,twh_top,twt_top,\
-                                        tft_top,tfb_top,sloc_top,ornt_top,qloc_top,pmatl,smatl,tmatl,eta_top)
+                                        tft_top,tfb_top,sloc_top,ornt_top,qloc_top,pmatl,smatl,tmatl,eta_top) """
 
 # Check the box for corner stiffener intersections
 """ check_my_box = Box_Checker.boxchecker(tp_bot,hw_top,tf_top,B_side,nstiff_side,tw_side,bf_side, test_panel_bot, test_panel_side, test_panel_top)
@@ -123,7 +123,7 @@ check_my_box.check_corner_stiffs()
 check_my_box.check_geometry() """
 
 # Create the midship section    
-structure = midship_section.Midship_Section([test_panel_bot,test_panel_side,test_panel_top],0)
+structure = midship_section.Midship_Section([test_panel_bot],0)
         
 #get section data
 '''
@@ -154,11 +154,11 @@ print ("here's PC: ", PC)
 
 
 #check if the geometry is valid for each individual panel
-valid_bot = test_panel_bot.geoValid()
+""" valid_bot = test_panel_bot.geoValid()
 valid_side = test_panel_side.geoValid()
-valid_top = test_panel_top.geoValid()
+valid_top = test_panel_top.geoValid() """
 
 #get pressure for allowable permanent set
-press = Allowable_Permanent_Set.Allowable_Permanent_Set(0, 10)
+""" press = Allowable_Permanent_Set.Allowable_Permanent_Set(0, 10)
 press_bot = press._p_aps(test_panel_bot)
-print ("here's the pressure: ", press_bot)
+print ("here's the pressure: ", press_bot) """
